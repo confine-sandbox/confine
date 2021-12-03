a8ea4583de608d58c978ec858a19e12340f13c35
Paul Frazee <pfrazee@gmail.com>
2021-12-03 15:07:02 -0600

    Add simple wasm test

---
+++ test/wasm-no-sandbox.js
@@ -0,0 +1,14 @@
+import ava from 'ava'
+import { genIsBlocked } from './_util/index.js'
+
+const isBlocked = genIsBlocked({runtime: 'simplewasm-confine-runtime', noSandbox: true})
+
+function allow (program) {
+  ava(`${program} allowed`, isBlocked, program, false)
+}
+
+function deny (program) {
+  ava(`${program} denied`, isBlocked, program, true)
+}
+
+allow('fizzbuzz.wasm')
